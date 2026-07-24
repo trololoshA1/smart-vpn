0462f44dce25f8cac90502f68f7f69ebd71502ab
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 05:46:18 +0400

    Update ip.go

--- server/ip.go
+++ server/ip.go
@@ -1,22 +1,17 @@
-package server
+package core
 
 import (
     "net"
 )
 
-func ProcessIPPacket(packet []byte) []byte {
+func ProcessIPPacket(packet []byte) (dstIP net.IP, dstPort int, proto int) {
     if len(packet) < 20 {
-        return nil
+        return nil, 0, 0
     }
 
-    proto := packet[9]
+    proto = int(packet[9])
+    dstIP = net.IP(packet[16:20])
+    dstPort = int(packet[22])<<8 | int(packet[23])
 
-    switch proto {
-    case 6: // TCP
-        return HandleTCP(packet)
-    case 17: // UDP
-        return HandleUDP(packet)
-    default:
-        return nil
-    }
+    return
 }
