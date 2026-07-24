9641d38178058d23a5cac4fa22c10e5fee6769bd
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 05:49:09 +0400

    Update udp.go

--- server/udp.go
+++ server/udp.go
@@ -1,31 +1,38 @@
-package core
+package server
 
 import (
+    "encoding/binary"
     "net"
 )
 
-func HandleUDPLocal(packet []byte) ([]byte, error) {
-    dstIP, dstPort, _ := ProcessIPPacket(packet)
-    if dstIP == nil {
-        return nil, nil
+func HandleUDP(packet []byte) []byte {
+    if len(packet) < 40 {
+        return nil
     }
 
-    conn, err := net.Dial("udp", net.JoinHostPort(dstIP.String(), string(dstPort)))
+    dstIP := net.IP(packet[16:20])
+    dstPort := binary.BigEndian.Uint16(packet[22:24])
+
+    conn, err := net.Dial("udp", net.JoinHostPort(dstIP.String(), stringPort(dstPort)))
     if err != nil {
-        return nil, err
+        return nil
     }
     defer conn.Close()
 
     _, err = conn.Write(packet)
     if err != nil {
-        return nil, err
+        return nil
     }
 
     buf := make([]byte, 65535)
     n, err := conn.Read(buf)
     if err != nil {
-        return nil, err
+        return nil
     }
 
-    return buf[:n], nil
+    return buf[:n]
+}
+
+func stringPort(p uint16) string {
+    return net.Itoa(int(p))
 }
