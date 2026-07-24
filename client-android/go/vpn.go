4741b24f20c6110240cb87e5e0bfddcdb7405ecf
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 05:19:38 +0400

    Update vpn.go

--- client-android/go/vpn.go
+++ client-android/go/vpn.go
@@ -1,24 +1,17 @@
 package mobile
 
 import (
+    "smart-vpn/subscriptions"
     "smart-vpn/core"
     "smart-vpn/geoip"
     "smart-vpn/adblock"
-    "smart-vpn/subscriptions"
 )
 
 var vpnCore *core.VPN
 
 func Init(geoPath string, adPath string, subsPath string, updateURL string) error {
-    geo, err := geoip.NewGeoIP(geoPath)
-    if err != nil {
-        return err
-    }
-
-    ad, err := adblock.NewAdBlock(adPath)
-    if err != nil {
-        return err
-    }
+    geo, _ := geoip.NewGeoIP(geoPath)
+    ad, _ := adblock.NewAdBlock(adPath)
 
     subs := subscriptions.NewManager()
     subs.Load(subsPath)
@@ -33,30 +26,6 @@ func AddSubscription(name, url string, auto bool, interval int) {
     vpnCore.SubsManager.AddSubscription(name, url, auto, interval)
 }
 
-func Connect() error {
-    return vpnCore.Connect()
-}
-
-func HandlePacket(packet []byte) ([]byte, error) {
-    return vpnCore.HandlePacket(packet)
-}
-
 func GetSubscriptions() []subscriptions.Subscription {
     return vpnCore.SubsManager.Subs
-}
-
-func CheckNodes() []subscriptions.Subscription {
-    vpnCore.SubsManager.CheckAll()
-    return vpnCore.SubsManager.Subs
-}
-
-func GetBestNode() *subscriptions.Subscription {
-    return vpnCore.SubsManager.Best()
-}
-
-func Status() string {
-    if vpnCore == nil {
-        return "Не инициализировано"
-    }
-    return "Работает"
 }
