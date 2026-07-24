dc1fcddd5c4381ef269789f3d9a365c6e228d414
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 05:47:36 +0400

    Update vpn.go

--- core/vpn.go
+++ core/vpn.go
@@ -5,72 +5,18 @@ import (
     "net"
 )
 
-type VPN struct {
-    Geo         *GeoIP
-    Ad          *AdBlock
-    SubsManager *subscriptions.Manager
-    Protocol    *Protocol
-    Handshake   *Handshake
-    Conn        net.Conn
-}
-
-func NewVPN(geo *GeoIP, ad *AdBlock, subs *subscriptions.Manager) *VPN {
-    return &VPN{
-        Geo:         geo,
-        Ad:          ad,
-        SubsManager: subs,
-    }
-}
-
-func (v *VPN) Connect() error {
-    node := v.SubsManager.BestNode()
-    if node == nil {
-        return errors.New("нет узлов")
-    }
-
-    // TCP соединение с сервером узла
-    conn, err := net.Dial("tcp", node.Address)
-    if err != nil {
-        return err
-    }
-    v.Conn = conn
-
-    // Handshake
-    hs, err := NewHandshake()
-    if err != nil {
-        return err
-    }
-    v.Handshake = hs
-
-    // Отправляем публичный ключ клиента
-    _, err = conn.Write(hs.ClientPub[:])
-    if err != nil {
-        return err
-    }
-
-    // Получаем публичный ключ сервера
-    var serverPub [32]byte
-    _, err = conn.Read(serverPub[:])
-    if err != nil {
-        return err
-    }
-
-    hs.SetServerPub(serverPub)
-    hs.ComputeSharedKey()
-
-    // Создаём протокол с общим ключом
-    v.Protocol = NewProtocol(hs.SharedKey)
-
-    return nil
-}
-
 func (v *VPN) HandlePacket(packet []byte) ([]byte, error) {
     if v.Protocol == nil {
         return nil, errors.New("нет протокола")
     }
 
-    nonce := v.Protocol.NewNonce()
+    dstIP, dstPort, proto := ProcessIPPacket(packet)
+    if dstIP == nil {
+        return nil, nil
+    }
 
+    // Шифруем и отправляем на сервер
+    nonce := v.Protocol.NewNonce()
     encrypted, err := v.Protocol.Encrypt(nonce, packet)
     if err != nil {
         return nil, err
@@ -78,7 +24,6 @@ func (v *VPN) HandlePacket(packet []byte) ([]byte, error) {
 
     encoded := EncodePacket(nonce, encrypted)
 
-    // Отправляем на сервер
     _, err = v.Conn.Write(encoded)
     if err != nil {
         return nil, err
