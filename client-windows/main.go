94fa4fb9b46e7f2175286ef43aa00a2afba61990
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 04:36:43 +0400

    Create client-windows/main.go

---
+++ client-windows/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+
+    "github.com/wailsapp/wails/v2"
+    "github.com/wailsapp/wails/v2/pkg/options"
+    "smart-vpn/client-windows/go"
+)
+
+func main() {
+    app := go.NewApp()
+
+    err := wails.Run(&options.App{
+        Title:  "SmartVPN",
+        Width:  600,
+        Height: 400,
+        Bind: []interface{}{
+            app,
+        },
+    })
+
+    if err != nil {
+        log.Fatal(err)
+    }
+}
