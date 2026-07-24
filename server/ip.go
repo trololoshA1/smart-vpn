edb50bb505bac1afd6e71af3f601f0e4721347cb
trololoshA1 <103339149+trololoshA1@users.noreply.github.com>
2026-07-24 05:51:04 +0400

    Update ip.go

--- server/ip.go
+++ server/ip.go
@@ -1,8 +1,39 @@
-package server
+package core
 
-func ProcessIPPacket(packet []byte) int {
+import (
+    "encoding/binary"
+    "net"
+)
+
+type IPInfo struct {
+    Proto   int
+    SrcIP   net.IP
+    DstIP   net.IP
+    SrcPort uint16
+    DstPort uint16
+}
+
+func ParseIP(packet []byte) *IPInfo {
     if len(packet) < 20 {
-        return 0
+        return nil
     }
-    return int(packet[9]) // 6 = TCP, 17 = UDP
+
+    proto := int(packet[9])
+    srcIP := net.IP(packet[12:16])
+    dstIP := net.IP(packet[16:20])
+
+    var srcPort, dstPort uint16
+
+    if proto == 6 || proto == 17 {
+        srcPort = binary.BigEndian.Uint16(packet[20:22])
+        dstPort = binary.BigEndian.Uint16(packet[22:24])
+    }
+
+    return &IPInfo{
+        Proto:   proto,
+        SrcIP:   srcIP,
+        DstIP:   dstIP,
+        SrcPort: srcPort,
+        DstPort: dstPort,
+    }
 }
